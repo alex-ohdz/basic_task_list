377b5399f32f990b2757292f4ff8e786e108d1e4
Alejandro Oramas H <alejandrooramash@gmail.com>
2024-06-22 11:14:01 -0400

    ok

---
+++ src/hooks/useIsMobile.js
@@ -0,0 +1,19 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1230);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return isMobile;
+}
