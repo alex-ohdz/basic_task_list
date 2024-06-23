f0f640f12a2fbd1234f080f56d136ff6cbb87396
Alejandro Oramas H <alejandrooramash@gmail.com>
2024-06-22 22:56:56 -0400

    qwee

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: parseInt(process.env.PG_PORT, 10), 
+  user: process.env.local.PG_USER,
+  host: process.env.local.PG_HOST,
+  database: process.env.local.PG_DATABASE,
+  password: process.env.local.PG_PASSWORD,
+  port: parseInt(process.env.local.PG_PORT, 10), 
 });
 
 export default pool;
